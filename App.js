e6b9089a03814421b808564183ed7cb3bdb9aa13
PurnimaNaik <naikpurnima9@gmail.com>
2019-08-07 12:28:45 -0400

    Map showing

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
+  StatusBar,Dimensions
 } from 'react-native';
 
 import {
@@ -31,49 +31,21 @@ const App = () => {
     <Fragment>
 
       <SafeAreaView>
+        <View style={styles.container}>
         <Map/>
+        </View>
+       
       </SafeAreaView>
     </Fragment>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height:Dimensions.get('window').height-80,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+
 });
 
 export default App;
--- src/mapScreen.js
+++ src/mapScreen.js
@@ -2,15 +2,43 @@
 
 import React from 'react';
 import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export class Map extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      <View>
-        <Text>HIIIIiiii</Text>
+        <View style={styles.container}>
+        <Text>Hiiii</Text>
+    <MapView
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+    
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    map: {
+        ...StyleSheet.absoluteFillObject,
+      },
+      container: {
+        ...StyleSheet.absoluteFillObject,
+        alignItems: 'center',
+      },
+
+});
 
 //  default Map;
