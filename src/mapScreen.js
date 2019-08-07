f6b4443e303db2691ba91e21014a490f91987d85
PurnimaNaik <naikpurnima9@gmail.com>
2019-08-07 11:49:56 -0400

    rn maps linked

---
+++ src/mapScreen.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import { View, StyleSheet, Text, Dimensions } from 'react-native';
+
+export class Map extends React.PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>HIIIIiiii</Text>
+      </View>
+    );
+  }
+}
+
+//  default Map;
